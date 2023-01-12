a420f364c74434639613e027898e751c11678087
Mike <michaelorejuelaramirez@gmail.com>
2023-01-12 11:44:51 -1200

    Updated routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import io from 'socket.io-client';
 
-const socket = io.connect("https://mike-orejuela-portfolio-server.onrender.com");
+const socket = io.connect("https://mike-orejuela-portfolio-server.onrender.com:3001");
 
 function App() {
   return (
